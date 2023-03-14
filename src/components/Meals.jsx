21549d7be41ba49c28051d655916bb32ba449378
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-14 04:27:25 +0000

    import icon library and create the button

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useGlobalContext } from '../context'
+import { BsHandThumbsUp } from 'react-icons/bs'
 
 const Meals = () => {
   const mealsData = useGlobalContext()
@@ -10,9 +11,10 @@ const Meals = () => {
           //deconstruct
           const { idMeal: id, strMeal: title, strMealThumb: img } = m
           return (<article className='single-meal' key={id}>
-            <img src={img} className="img" style={{ width: '200px' }} />
+            <img src={img} className="img" />
             <footer>
               <h5>{title}</h5>
+              <button className='like-btn'><BsHandThumbsUp /></button>
             </footer>
           </article>)
         })
