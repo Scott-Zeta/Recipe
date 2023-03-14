f050589447c54441d3adbb8f96d135c6c379b06b
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-14 04:17:22 +0000

    render imge and title in card

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,15 +1,23 @@
 import { useGlobalContext } from '../context'
 
 const Meals = () => {
-  const landing = useGlobalContext()
-  console.log(landing)
+  const mealsData = useGlobalContext()
+  console.log(mealsData)
   return (
-    <>
-      <h1>Meals</h1>
-      <div>{
-        landing.map(m=><p key={m.idMeal}>{m.strMeal}</p>)
-      }</div>
-    </>
+    <section className='section-center'>
+      {
+        mealsData.map(m => {
+          //deconstruct
+          const { idMeal: id, strMeal: title, strMealThumb: img } = m
+          return (<article className='single-meal' key={id}>
+            <img src={img} className="img" style={{ width: '200px' }} />
+            <footer>
+              <h5>{title}</h5>
+            </footer>
+          </article>)
+        })
+      }
+    </section>
   )
 }
 
