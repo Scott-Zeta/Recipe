eaf992cf12e4ef3a1851f8226c725c6b67e78287
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-14 02:09:45 +0000

    bugfix for consumer

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,12 +1,12 @@
-import {useContext} from 'react'
-import {AppContext} from '../context'
+import { useContext } from 'react'
+import { AppContext } from '../context'
 
 const Meals = () => {
   const landing = useContext(AppContext)
   return (
     <>
       <h1>Meals</h1>
-      <h5>landing</h5>
+      <h5>{landing}</h5>
     </>
   )
 }
