514c434544c148b3c75c3ec2c0e4ea6f422f0ace
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-14 06:14:50 +0000

    handle change and submit for search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,17 @@
-import {useState} from 'react'
-import {useGlobalContext} from '../context'
+import { useState } from 'react'
+import { useGlobalContext } from '../context'
 
 const Search = () => {
+  const [text, setText] = useState('')
+  const textChange = (e) => setText(e.target.value)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <header className='search-container'>
-      <form>
-        <input type='text' placeholder='What is your favorite?' className='form-input'/>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={text} onChange={textChange} placeholder='What is your favorite?' className='form-input' />
         <button type='submit' className='btn'>Search</button>
         <button type='button' className='btn btn-hipster'>Suprise me!</button>
       </form>
