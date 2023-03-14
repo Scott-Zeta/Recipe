47d0a28fd2116bb33f8b0933e1499c2f0ae5f562
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-14 02:55:57 +0000

    implement the fetch with useEffect

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=').then(response => response.json()).then(data => console.log(data)).catch(e => console.log('Error', e))
+  }, [])
+  //useEffect is like provide an isolated logic environment for the function inside
+
   return <AppContext.Provider value='testString'>
     {children}
   </AppContext.Provider>
