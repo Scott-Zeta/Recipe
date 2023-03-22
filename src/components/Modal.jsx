dde6ba98232661f2bb85f409513f99381e69c5c6
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-22 03:18:56 +0000

    implement and pass the meals info to modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,18 @@
 import { useGlobalContext } from "../context"
 
 const Modal = () => {
-  const { closeModal } = useGlobalContext()
+  const { selectedMeal, closeModal } = useGlobalContext()
+  const {idMeal,strMealThumb:image,strMeal:title,strInstructions:text,strSource:source} = selectedMeal
   return (
-    <aside className="modal-overlay" onClick={()=>closeModal()}>
+    <aside className="modal-overlay" onClick={() => closeModal()}>
       <div className="modal-container">
-        <h1>Modal</h1>
+        <img src={image} className="img modal-img"/>
+        <div className='modal-content'>
+        <h4>{title}</h4>
+          <p>Cooking Instructions</p>
+          <p>{text}</p>
+          <a href={source} target="_blank">Origninal Source</a>
+        </div>
       </div>
     </aside>
   )
